7bd084daec5d48618a1e78326f684f9a438c3ed0
Jonathan Huang <jon.hc.huang@gmail.com>
2015-09-22 20:36:58 -0400

    moved categorizing to ipynb

--- loader.py
+++ loader.py
@@ -10,9 +10,6 @@ class Person(object):
     def __init__(self):
         self.info = {}
         self.person_id = None
-        self.chronic = False
-        # ignore this -- i dont think anyone exists that has everything filled out
-        self.all_pop = True
 
 
 def load_info(filename, rows):
@@ -41,16 +38,6 @@ def load_info(filename, rows):
                     if code in export:
                         code = export[code]
                         rows[pid].info[code] = num(value)
-                        # jing check this
-                        if 'chronic_'in code:
-                            if not rows[pid].chronic:
-                                if rows[pid].info[code] is 1:
-                                    rows[pid].chronic = True
-                                elif rows[pid].info[code] is -9 or rows[pid].info[code] is -8:
-                                    rows[pid].chronic = -1
-                            # i don't think anyone exists like this who is not_chronic
-                            if rows[pid].all_pop and 'chronic_cancer_' not in code and rows[pid].info[code] is -1:
-                                rows[pid].all_pop = False
             else:
                 logging.exception("length check failed :  " + str(row))
     logging.info("number of rows: " + str(len(rows)))
